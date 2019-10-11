b1da75f83a209a93f4e302ff8256cc526a057d10
Griggorii <40373174+Griggorii@users.noreply.github.com>
2019-10-11 16:14:45 +0800

    Add files via upload

---
+++ Install_xorg_fix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ 
+sudo cp 20-intel.conf /usr/share/X11/xorg.conf.d && sudo cp xorg.conf /etc/X11 && sudo cp xorg.conf.failsafe /etc/X11 && cp .drirc ~ && notify-send -i info Information "перезагрузитесь что бы изменения вступили в силу Xorg fix тиринг obs perfomance please reboot restart by Griggorii my donate https://money.yandex.ru/to/410014999913799 "
