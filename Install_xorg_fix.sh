3618cc610e5f08f003867b34b987601de5a4cd0d
Griggorii <40373174+Griggorii@users.noreply.github.com>
2019-10-20 23:28:50 +0800

    Delete Install_xorg_fix.sh

--- Install_xorg_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-sudo cp 20-intel.conf /usr/share/X11/xorg.conf.d && sudo cp xorg.conf /etc/X11 && sudo cp xorg.conf.failsafe /etc/X11 && notify-send -i info Information "перезагрузитесь что бы изменения вступили в силу Xorg fix тиринг obs perfomance please reboot restart by Griggorii my donate https://money.yandex.ru/to/410014999913799 " && cd ~ && cat << EOF > .drirc
-<device screen="0" driver="dri2">
-        <application name="Default">
-                <option name="vblank_mode" value="1"/>
-        </application>
-</device>
